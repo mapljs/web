74804d5d689b5ba31736dcd633fb4d523db80ff1
aquapi <aquaplmc@gmail.com>
2025-06-20 21:30:14 +0700

    Update

--- bench/api/startup.ts
+++
@@ -1,4 +0,0 @@
-import frameworks from './.out/index.js';
-import { relative } from 'node:path';
-
-await Bun.$`hyperfine ${frameworks.map((path) => 'node ./' + relative('.', path))} --runs 50 --warmup 10 --show-output`;
