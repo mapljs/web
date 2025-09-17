1143d872e2b251931c0f112790c639097709ad6d
aquapi <aquaplmc@gmail.com>
2025-09-17 17:43:16 +0700

    ok

--- tests/aot/1.js
+++ tests/aot/1.js
@@ -5,55 +5,9 @@ import hydrateRouter from '../../lib/compiler/aot.js';
 hydrateRouter(app);
 
 import { hydrate } from 'runtime-compiler/hydrate';
-((_, _1, _2, _3, _4, _5, _6) => {
-  var __0 = (r) => console.log(r.method, r.url),
-    __1 = ['access-control-max-age', '60000'],
-    __2 = ['access-control-allow-origin', '*'],
-    __3 = (r, hd) => {
-      hd.push(__2);
-      r.method === 'OPTIONS' && hd.push(__1);
-    },
-    __4 = ['x-powered-by', '@mapl/web'],
-    __5 = (() => {
-      var t = ['text/html', 'application/json'].map((c) => ['Content-Type', c]),
-        [h, j] = t,
-        [oh, oj] = t.map((c) => ({ headers: [c] })),
-        [n, b] = [404, 400].map((s) => new Response(null, { status: s }));
-      return (r) => {
-        let u = r.url,
-          s = u.indexOf('/', 12) + 1,
-          e = u.indexOf('?', s),
-          p = e === -1 ? u.slice(s) : u.slice(s, e);
-        if (r.method === 'POST') {
-          if (p === 'api') {
-            let hd = [],
-              c = { status: 200, req: r, headers: hd };
-            __3(r, hd);
-            _2(c);
-            c.id = _3();
-            hd.push(__4);
-            return (async () => {
-              c.body = await _5(c);
-              return new Response(_6(c), c);
-            })();
-          }
-        }
-        if (p === 'path') {
-          let hd = [],
-            c = { status: 200, req: r, headers: hd };
-          __3(r, hd);
-          _2(c);
-          c.id = _3();
-          hd.push(__4);
-          return new Response(_4(c), c);
-        }
-        return n;
-      };
-    })();
-  _.push(__0, __5);
-})(...hydrate());
+((_,_1,_2,_3,_4,)=>{var __0=(()=>{var t=["text/html","application/json"].map(c=>["Content-Type",c]),[h,j]=t,[oh,oj]=t.map(c=>({headers:[c]})),[n,b]=[404,400].map(s=>new Response(null,{status:s}));return(r)=>{if(r.method==="GET"){let u=r.url,s=u.indexOf("/",12)+1,e=u.indexOf("?",s),p=e===-1?u.slice(s):u.slice(s,e);if(p===""){return new Response(_2())}{let l=p.length;if(l>0){if(p[0]==="u"){if(l>5)if(p.startsWith("ser/",1)){if(p.indexOf("/",5)===-1){let q0=p.slice(5);return new Response(_3(,q1))}}}}}}else if(r.method==="POST"){let u=r.url,s=u.indexOf("/",12)+1,e=u.indexOf("?",s),p=e===-1?u.slice(s):u.slice(s,e);if(p==="body"){let hd=[],c={status:200,req:r,headers:hd};return (async()=>{hd.push(j);return new Response(JSON.stringify(await _4(c)),c)})()}}return n}})();_.push(__0,)})(...hydrate());
 
 import { getDependency } from 'runtime-compiler';
 export default {
-  fetch: getDependency(1),
+  fetch: getDependency(0)
 };
