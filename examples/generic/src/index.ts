ab7af0862c666a095799b3154917573899a85712
aquapi <aquaplmc@gmail.com>
2025-09-28 15:48:43 +0700

    update

--- examples/generic/src/index.ts
+++ examples/generic/src/index.ts
@@ -1,4 +1,19 @@
 import { cors, handle, router, staticHeaders } from '@mapl/web';
 import api from './api.ts';
 
-export default api
+export default router(
+  [
+    cors.init('*', [cors.maxAge(60000)]),
+    staticHeaders({
+      'x-powered-by': '@mapl/web',
+    }),
+  ],
+  [
+    handle.any('/path', () => '' + performance.now(), {
+      type: handle.text,
+    }),
+  ],
+  {
+    '/api': api
+  },
+);
